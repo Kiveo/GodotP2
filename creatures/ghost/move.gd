b2f13b5bb66d6b1646e0d8c9453929aff10d3709
Maverick <mavericktesini@gmail.com>
2024-08-31 02:33:44 -0400

    improve owl movement mechanics

--- creatures/ghost/move.gd
+++ creatures/ghost/move.gd
@@ -3,6 +3,8 @@ extends State
 @export var idle_state: State
 @export var attack_state: State
 
+#var tween: Tween
+
 func enter() -> void:
 	super()
 
@@ -12,7 +14,13 @@ func process_input(_event: InputEvent) -> State:
 	return null
 
 func process_physics(_delta: float) -> State:
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if !Input.is_action_pressed("hold_player"):
+		var tween = parent.create_tween()
+		var target_pos = get_tree().get_nodes_in_group('owl_position')[0].global_position
+		tween = parent.create_tween()
+		tween.tween_property(parent, "position", target_pos, 0.25)
+	
 	parent.velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * move_speed
 	parent.animated_sprite_2d.flip_h = direction.x < 0
 	parent.move_and_slide()
