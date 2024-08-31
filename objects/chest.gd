5419f7f00b5085d04a9c59e7f4d1d956ee6feffc
Maverick <mavericktesini@gmail.com>
2024-08-31 17:25:09 -0400

    play chest audio

--- objects/chest.gd
+++ objects/chest.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var chest_animations: AnimationPlayer = %ChestAnimations
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body is not Player: return
 	HUD.update_message("Treasure Found!")
+	audio_stream_player.play(3.5)
+	await audio_stream_player.finished
+	
 	chest_animations.play("open")
 	await chest_animations.animation_finished
 	timer.start()
