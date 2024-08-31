befc84fc92853446725eefe70ef7ce6b1445e0c6
Maverick <mavericktesini@gmail.com>
2024-08-31 16:24:08 -0400

    add owl attack audio

--- creatures/ghost/attack.gd
+++ creatures/ghost/attack.gd
@@ -5,15 +5,27 @@ extends State
 @export var attack_state: State
 @export_category('Custom')
 @export var attack_speed: float
-
 @onready var hit_box_collision_shape: CollisionShape2D = %HitBoxCollisionShape
-
 var initial_velocity: Vector2
 var current_attack_time: float = 0.0
 const ATTACK_TIME: float = 1.0
 var attacking: bool = false
 var reloaded: bool = false
 var tween: Tween
+# audio variables
+@export var attack_commands: Array[AudioStream]
+@onready var owl_audio: AudioStreamPlayer = %OwlAudio
+var attack_commands_index: int = 0
+
+func play_attack_audio() -> void:
+	if owl_audio.playing: 
+		print("already playing")
+		return
+	owl_audio.stream = attack_commands[attack_commands_index]
+	owl_audio.play()
+	attack_commands_index += 1
+	if attack_commands_index > 2:
+		attack_commands_index = 0
 
 func enter() -> void:
 	super()
@@ -24,6 +36,8 @@ func enter() -> void:
 		initial_velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down").sign()
 	current_attack_time = ATTACK_TIME
 	reloaded = false
+	#audio 
+	play_attack_audio()
 
 func process_input(_event: InputEvent) -> State:
 	if !attacking and Input.is_action_just_pressed('attack'):
