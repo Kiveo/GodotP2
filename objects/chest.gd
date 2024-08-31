e678d0f25ebc2958a2ce2dbf00553b01b89d7cd9
Maverick <mavericktesini@gmail.com>
2024-08-31 17:20:57 -0400

    implement chest sprites and animation

--- objects/chest.gd
+++ objects/chest.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var chest_animations: AnimationPlayer = %ChestAnimations
 
-func _on_body_entered(player: Player) -> void:
+func _on_body_entered(body: CharacterBody2D) -> void:
+	if body is not Player: return
 	HUD.update_message("Treasure Found!")
-	#TODO tween stuff
+	chest_animations.play("open")
+	await chest_animations.animation_finished
 	timer.start()
 
 func _on_timer_timeout() -> void:
